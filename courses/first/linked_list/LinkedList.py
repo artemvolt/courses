a7381df5e2734c99b2ee1a528f0fc176a06d1f4b
Artem Volt <avoltornistyy@beeline.ru>
2022-06-30 08:47:25 +0300

    Task 1.1 and 1.2. Method delete with flag all.

---
+++ courses/first/linked_list/LinkedList.py
@@ -0,0 +1,95 @@
+class Node:
+
+    def __init__(self, v):
+        self.value = v
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+        else:
+            self.tail.next = item
+        self.tail = item
+
+    def print_all_nodes(self):
+        node = self.head
+        while node != None:
+            print(node.value)
+            node = node.next
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        return []  # здесь будет ваш код
+
+    def delete(self, val, all=False):
+        head = self.head
+        prev = None
+
+        count = 0
+        countStop = self.head
+        while countStop is not None:
+            count += 1
+            countStop = countStop.next
+
+        if count == 0:
+            return
+
+        if count == 1 and self.head.value == val:
+            self.head = None
+            self.tail = None
+            return
+
+        while head is not None:
+            if head.value == val:
+                if head == self.head:
+                    self.head = head.next
+                    prev = None
+                    head = head.next
+
+                    if not all:
+                        head = None
+
+                    continue
+
+                if head == self.tail:
+                    prev.next = None
+                    self.tail = prev
+                    prev = head
+                    head = None
+
+                    if not all:
+                        head = None
+
+                    continue
+
+                prev.next = head.next
+                prev = prev
+                head = head.next
+                if not all:
+                    head = None
+            else:
+                prev = head
+                head = head.next
+
+    def clean(self):
+        pass  # здесь будет ваш код
+
+    def len(self):
+        return 0  # здесь будет ваш код
+
+    def insert(self, afterNode, newNode):
+        pass  # здесь будет ваш код
---
+++ tests/courses/first/linked_list/LinkedListTestDelete.py
@@ -0,0 +1,118 @@
+import unittest
+import sys
+
+sys.path.append('/var/www/courses')
+
+from courses.first.linked_list.LinkedList import Node, LinkedList
+
+
+class LinkedListTestDelete(unittest.TestCase):
+    def test_delete_one(self):
+        n1 = Node(30)
+        linked_list = LinkedList()
+        linked_list.add_in_tail(n1)
+
+        linked_list.delete(n1.value)
+        self.assertIsNone(linked_list.head)
+        self.assertIsNone(linked_list.tail)
+
+    def test_delete_first(self):
+        n1 = Node(30)
+        n2 = Node(44)
+        linked_list = LinkedList()
+        linked_list.add_in_tail(n1)
+        linked_list.add_in_tail(n2)
+
+        linked_list.delete(n1.value)
+        self.assertEqual(n2, linked_list.head)
+        self.assertEqual(n2, linked_list.tail)
+
+        n3 = Node(30)
+        n4 = Node(44)
+        n5 = Node(44)
+        linked_list = LinkedList()
+        linked_list.add_in_tail(n3)
+        linked_list.add_in_tail(n4)
+        linked_list.add_in_tail(n5)
+
+        linked_list.delete(n3.value)
+        self.assertEqual(n4, linked_list.head)
+        self.assertIsNotNone(linked_list.head.next)
+        self.assertEqual(n5, linked_list.head.next)
+        self.assertEqual(n5, linked_list.tail)
+
+    def test_delete_last(self):
+        n1 = Node(30)
+        n2 = Node(44)
+        linked_list = LinkedList()
+        linked_list.add_in_tail(n1)
+        linked_list.add_in_tail(n2)
+
+        linked_list.delete(n2.value)
+        self.assertEqual(n1, linked_list.head)
+        self.assertEqual(n1, linked_list.tail)
+
+        n3 = Node(30)
+        n4 = Node(44)
+        n5 = Node(45)
+        linked_list = LinkedList()
+        linked_list.add_in_tail(n3)
+        linked_list.add_in_tail(n4)
+        linked_list.add_in_tail(n5)
+
+        linked_list.delete(n5.value)
+        self.assertEqual(n3, linked_list.head)
+        self.assertIsNotNone(linked_list.head.next)
+        self.assertEqual(n4, linked_list.head.next)
+        self.assertEqual(n4, linked_list.tail)
+
+    def test_delete_medium(self):
+        n3 = Node(30)
+        n4 = Node(44)
+        n5 = Node(45)
+        linked_list = LinkedList()
+        linked_list.add_in_tail(n3)
+        linked_list.add_in_tail(n4)
+        linked_list.add_in_tail(n5)
+
+        linked_list.delete(n4.value)
+        self.assertEqual(n3, linked_list.head)
+        self.assertIsNotNone(linked_list.head.next)
+        self.assertEqual(n5, linked_list.head.next)
+        self.assertEqual(n5, linked_list.tail)
+
+    def test_delete_with_out_all(self):
+        n3 = Node(30)
+        n4 = Node(33)
+        n5 = Node(44)
+        n6 = Node(33)
+        linked_list = LinkedList()
+        linked_list.add_in_tail(n3)
+        linked_list.add_in_tail(n4)
+        linked_list.add_in_tail(n5)
+        linked_list.add_in_tail(n6)
+
+        linked_list.delete(n4.value)
+        self.assertIsNotNone(linked_list.find(33))
+        self.assertEqual(n3, linked_list.head)
+        self.assertEqual(n5, linked_list.head.next)
+        self.assertEqual(n6, linked_list.tail)
+
+    def test_delete_with_all(self):
+        n3 = Node(30)
+        n4 = Node(33)
+        n5 = Node(44)
+        n6 = Node(33)
+        linked_list = LinkedList()
+        linked_list.add_in_tail(n3)
+        linked_list.add_in_tail(n4)
+        linked_list.add_in_tail(n5)
+        linked_list.add_in_tail(n6)
+
+        linked_list.delete(n4.value, True)
+        self.assertIsNone(linked_list.find(33))
+        self.assertEqual(n3, linked_list.head)
+        self.assertEqual(n5, linked_list.head.next)
+        self.assertEqual(n5, linked_list.tail)
+
+
