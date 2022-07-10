a0f02c0aac8c42d27eaaceb2c6efb0c4ca86181d
Artem Volt <avoltornistyy@beeline.ru>
2022-07-10 13:42:41 +0300

    Dyn array. insert el.

---
+++ courses/first/dynamic_arrays/DynArray.py
@@ -0,0 +1,71 @@
+import ctypes
+
+
+class DynArray:
+
+    def __init__(self):
+        self.count = 0
+        self.capacity = 16
+        self.array = self.make_array(self.capacity)
+
+    def __len__(self):
+        return self.count
+
+    # noinspection PyMethodMayBeStatic
+    def make_array(self, new_capacity):
+        return (new_capacity * ctypes.py_object)()
+
+    def __getitem__(self, i):
+        if i < 0 or i >= self.count:
+            raise IndexError('Index is out of bounds')
+        return self.array[i]
+
+    def resize(self, new_capacity):
+        new_array = self.make_array(new_capacity)
+        for i in range(self.count):
+            new_array[i] = self.array[i]
+        self.array = new_array
+        self.capacity = new_capacity
+
+    def append(self, itm):
+        if self.count == self.capacity:
+            self.resize(2 * self.capacity)
+        self.array[self.count] = itm
+        self.count += 1
+
+    def insert(self, i, itm):
+        # добавляем объект itm в позицию i, начиная с 0
+        if i < 0 or i > self.count:
+            raise IndexError('Index is out of bounds')
+
+        if i == self.count:
+            self.append(itm)
+            return
+
+        if len(self) == 0:
+            self.array[i] = itm
+            self.count += 1
+            return
+
+        size = self.capacity
+        if self.count == self.capacity:
+            size *= 2
+            self.resize(size)
+
+        current = None
+        for k in range(i, self.count):
+            if current is None:
+                current = self.array[k]
+                self.array[k] = itm
+            else:
+                current_save = self.array[k]
+                self.array[k] = current
+                current = current_save
+                if k == (self.count - 1):
+                    self.array[k + 1] = current
+
+        self.count += 1
+
+    def delete(self, i):
+        # удаляем объект в позиции i
+        pass
---
+++ tests/courses/first/dynamic_arrays/DynArrayTest.py
@@ -0,0 +1,90 @@
+import sys
+import unittest
+
+from courses.first.dynamic_arrays.DynArray import DynArray
+
+sys.path.append('/var/www/courses')
+
+
+class DynArrayTest(unittest.TestCase):
+
+    def test_append(self):
+        dyn = DynArray()
+
+        for i in range(1, 17):
+            dyn.append(1)
+
+        self.assertEqual(16, len(dyn))
+        self.assertEqual(16, dyn.count)
+        self.assertEqual(16, dyn.capacity)
+
+        dyn.append(1)
+        self.assertEqual(17, len(dyn))
+        self.assertEqual(17, dyn.count)
+        self.assertEqual(32, dyn.capacity)
+
+    def test_insert(self):
+        dyn = DynArray()
+
+        self.assertRaises(IndexError, dyn.insert, 32, 1)
+        self.assertRaises(IndexError, dyn.insert, 1, 1)
+
+        # [1]
+        dyn.insert(0, 1)
+
+        self.assertEqual(1, dyn.count)
+        self.assertEqual(1, len(dyn))
+        self.assertEqual(1, dyn[0])
+
+        # [1,2]
+        dyn.insert(1, 2)
+        self.assertEqual(2, dyn.count)
+        self.assertEqual(2, len(dyn))
+        self.assertEqual(1, dyn[0])
+        self.assertEqual(2, dyn[1])
+
+        # [1,2,3]
+        dyn.insert(2, 3)
+        self.assertEqual(3, dyn.count)
+        self.assertEqual(3, len(dyn))
+        self.assertEqual(1, dyn[0])
+        self.assertEqual(2, dyn[1])
+        self.assertEqual(3, dyn[2])
+
+        # [1, 4, 2, 3]
+        dyn.insert(1, 4)
+        self.assertEqual(4, dyn.count)
+        self.assertEqual(4, len(dyn))
+        self.assertEqual(1, dyn[0])
+        self.assertEqual(4, dyn[1])
+        self.assertEqual(2, dyn[2])
+        self.assertEqual(3, dyn[3])
+
+        # [1, 5, 4, 2, 3]
+        dyn.insert(1, 5)
+        self.assertEqual(5, dyn.count)
+        self.assertEqual(5, len(dyn))
+        self.assertEqual(16, dyn.capacity)
+        self.assertEqual(1, dyn[0])
+        self.assertEqual(5, dyn[1])
+        self.assertEqual(4, dyn[2])
+        self.assertEqual(2, dyn[3])
+        self.assertEqual(3, dyn[4])
+
+    def test_insert_with_more_capacity(self):
+        dyn = DynArray()
+
+        for i in range(1, 17):
+            dyn.append(i)
+
+        self.assertEqual(16, len(dyn))
+        dyn.insert(1, 105)
+
+        self.assertEqual(17, len(dyn))
+        self.assertEqual(17, dyn.count)
+        self.assertEqual(32, dyn.capacity)
+        self.assertEqual(1, dyn[0])
+        self.assertEqual(105, dyn[1])
+        self.assertEqual(2, dyn[2])
+        self.assertEqual(3, dyn[3])
+        self.assertEqual(4, dyn[4])
