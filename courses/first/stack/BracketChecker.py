1f75dfb4cd8dccbc2bc2c8de5750831dc8d7bb66
Artem Volt <avoltornistyy@beeline.ru>
2022-07-17 19:53:08 +0300

    Bracker checker.

---
+++ courses/first/stack/BracketChecker.py
@@ -0,0 +1,21 @@
+from tokenize import String
+
+from courses.first.stack.Stack import Stack
+
+
+def bracket_check(bracket: String):
+    if len(bracket) % 2 != 0:
+        return False
+
+    stack = Stack()
+
+    for item in bracket:
+        if item == '(':
+            stack.push(item)
+        else:
+            if stack.size() == 0:
+                return False
+            else:
+                stack.pop()
+
+    return True
---
+++ tests/courses/first/stack/BracketCheckerTest.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+
+from courses.first.stack.BracketChecker import bracket_check
+
+sys.path.append('/var/www/courses')
+
+
+class BracketCheckerTest(unittest.TestCase):
+    def test_check(self):
+        self.assertFalse(bracket_check("())("))
+        self.assertFalse(bracket_check("))(("))
+        self.assertFalse(bracket_check("((())"))
+        self.assertTrue(bracket_check("()(())"))
+        self.assertTrue(bracket_check("((())())"))
+
