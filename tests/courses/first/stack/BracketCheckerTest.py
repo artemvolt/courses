110b9296dc6e5f1231c2e4a56d7ec5f11911fbd2
Artem Volt <avoltornistyy@beeline.ru>
2022-07-17 19:54:12 +0300

    Additional test.

--- tests/courses/first/stack/BracketCheckerTest.py
+++ tests/courses/first/stack/BracketCheckerTest.py
@@ -10,6 +10,7 @@ class BracketCheckerTest(unittest.TestCase):
     def test_check(self):
         self.assertFalse(bracket_check("())("))
         self.assertFalse(bracket_check("))(("))
+        self.assertFalse(bracket_check("))(("))
         self.assertFalse(bracket_check("((())"))
         self.assertTrue(bracket_check("()(())"))
         self.assertTrue(bracket_check("((())())"))
