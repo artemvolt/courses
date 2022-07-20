ff4aca728ef0a51d6299aedeed6c44bde9a12c07
Artem Volt <avoltornistyy@beeline.ru>
2022-07-20 08:32:29 +0300

    Add -, / to postfix calc.

--- courses/first/stack/postfix_calc.py
+++ courses/first/stack/postfix_calc.py
@@ -1,22 +1,34 @@
 from tokenize import String
+import operator
+import sys
 
 from courses.first.stack.Stack import Stack
 
+sys.path.append('/var/www/courses')
 
-def postfix_calc(input_raw: String):
+
+def postfix_calc(input_raw: String) -> String:
     if len(input_raw) == 0:
         raise AttributeError("Передана на вход пустая строка")
 
     numbers_stack = Stack()
+    ops = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.floordiv
+    }
 
     for el in input_raw:
-        if el == "+" or el == '*':
+        if el in ops:
             if not numbers_stack.size() == 2:
                 raise AttributeError("Для операции необходимо два операнда")
-            if el == "+":
-                numbers_stack.push(numbers_stack.pop() + numbers_stack.pop())
-            if el == "*":
-                numbers_stack.push(numbers_stack.pop() * numbers_stack.pop())
+
+            func_math = ops[el]
+            first = numbers_stack.pop()
+            second = numbers_stack.pop()
+            result = func_math(second, first)
+            numbers_stack.push(result)
 
         if el.isdigit():
             numbers_stack.push(int(el))
@@ -24,4 +36,4 @@ def postfix_calc(input_raw: String):
         if el == '=':
             return numbers_stack.peek()
 
-
+    return None
--- tests/courses/first/stack/postfix_calc_test.py
+++ tests/courses/first/stack/postfix_calc_test.py
@@ -1,12 +1,12 @@
 import sys
 import unittest
 
-from courses.first.stack.PostfixCalc import postfix_calc
+from courses.first.stack.postfix_calc import postfix_calc
 
 sys.path.append('/var/www/courses')
 
 
-class PostfixCalcTest(unittest.TestCase):
+class postfix_calc_test(unittest.TestCase):
     def test_size(self):
         with self.assertRaises(AttributeError):
             self.assertEqual(9, postfix_calc("1 + 2 * 3 ="))
@@ -17,3 +17,6 @@ class PostfixCalcTest(unittest.TestCase):
         self.assertEqual(9, postfix_calc("1 2 + 3 * ="))
         self.assertEqual(59, postfix_calc("8 2 + 5 * 9 + ="))
         self.assertIsNone(postfix_calc("8 2 + 5 * 9 +"))
+        self.assertEqual(50, postfix_calc("8 2 + 5 * ="))
+        self.assertEqual(10, postfix_calc("8 2 + 5 * 5 / ="))
+        self.assertEqual(45, postfix_calc("8 2 + 5 * 5 - ="))
