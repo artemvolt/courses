eb57a9b745fbf7d5c83ad539958441dccb0eb81f
Artem Volt <avoltornistyy@beeline.ru>
2022-07-20 12:57:33 +0300

    Bracket checker.

--- tests/courses/first/stack/bracket_checker_test.py
+++ tests/courses/first/stack/bracket_checker_test.py
@@ -1,12 +1,12 @@
 import unittest
 import sys
 
-from courses.first.stack.BracketChecker import bracket_check
+from courses.first.stack.bracket_checker import bracket_check
 
 sys.path.append('/var/www/courses')
 
 
-class BracketCheckerTest(unittest.TestCase):
+class bracker_checker_test(unittest.TestCase):
     def test_check(self):
         self.assertFalse(bracket_check("())("))
         self.assertFalse(bracket_check("))(("))
