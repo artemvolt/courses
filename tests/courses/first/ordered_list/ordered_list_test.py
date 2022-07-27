84b79838ee5e9dcf5b34e00ed94c4d80cfca682f
Artem Volt <avoltornistyy@beeline.ru>
2022-07-27 08:46:55 +0300

    Delete all.

--- tests/courses/first/ordered_list/ordered_list_test.py
+++ tests/courses/first/ordered_list/ordered_list_test.py
@@ -186,31 +186,102 @@ class ordered_list_test(unittest.TestCase):
         list.add(2)
         self.assertEqual(2, list.len())
 
-    def test_delete(self):
+    def test_delete_from_head(self):
         list = OrderedList(True)
         list.add(1)
-        list.delete(1)
-        self.assertEqual(0, list.len())
-        self.assertIsNone(list.head)
-        self.assertIsNone(list.tail)
-        list.add(1)
-        list.add(2)
-        list.delete(1)
-        self.assertEqual(2, list.head.value)
-        self.assertEqual(2, list.tail.value)
-        list.add(1)
-        list.delete(2)
-        self.assertEqual(1, list.head.value)
-        self.assertEqual(1, list.tail.value)
         list.add(2)
         list.add(3)
+        list.delete(1)
 
-        # 1 - 3
+        n1 = list.head
+        n2 = n1.next
+        self.assertEqual(2, n1.value)
+        self.assertEqual(3, n2.value)
+        self.assertEqual(n1, list.head)
+        self.assertEqual(n2, n1.next)
+        self.assertEqual(n2, list.tail)
+
+    def test_delete_from_tail(self):
+        list = OrderedList(True)
+        list.add(1)
+        list.add(2)
+        list.add(3)
+        list.delete(3)
+
+        n1 = list.head
+        n2 = n1.next
+        self.assertEqual(1, n1.value)
+        self.assertEqual(2, n2.value)
+        self.assertEqual(n1, list.head)
+        self.assertEqual(n2, n1.next)
+        self.assertEqual(n2, list.tail)
+        self.assertIsNone(n2.next)
+        self.assertIsNone(n1.prev)
+
+    def test_delete_from_middle(self):
+        list = OrderedList(True)
+        list.add(1)
+        list.add(2)
+        list.add(3)
         list.delete(2)
+
         n1 = list.head
         n2 = n1.next
         self.assertEqual(1, n1.value)
         self.assertEqual(3, n2.value)
+        self.assertEqual(n1, list.head)
+        self.assertEqual(n2, n1.next)
+        self.assertEqual(n2, list.tail)
+        self.assertIsNone(n2.next)
+        self.assertIsNone(n1.prev)
+
+    def test_delete_none_existing(self):
+        list = OrderedList(True)
+        list.add(1)
+        list.add(2)
+        list.add(3)
+        list.delete(4)
+
+        n1 = list.head
+        n2 = n1.next
+        n3 = n2.next
+        self.assertEqual(1, n1.value)
+        self.assertEqual(2, n2.value)
+        self.assertEqual(3, n3.value)
+        self.assertEqual(n1, list.head)
+        self.assertEqual(n2, n1.next)
+        self.assertEqual(n1, n2.prev)
+        self.assertEqual(n3, n2.next)
+        self.assertEqual(n2, n3.prev)
+        self.assertEqual(n3, list.tail)
+        self.assertIsNone(n3.next)
+        self.assertIsNone(n1.prev)
+
+    def test_delete_in_empty(self):
+        list = OrderedList(True)
+        list.delete(4)
+
+        self.assertIsNone(list.head)
+        self.assertIsNone(list.tail)
+
+    def test_delete_double(self):
+        list = OrderedList(True)
+        list.add(1)
+        list.add(2)
+        list.add(2)
+        list.add(4)
+
+        list.delete(2)
+        n1 = list.head
+        n2 = n1.next
+        self.assertEqual(1, n1.value)
+        self.assertEqual(4, n2.value)
+        self.assertEqual(n2, n1.next)
+        self.assertEqual(n1, n2.prev)
+        self.assertEqual(n1, list.head)
+        self.assertEqual(n2, list.tail)
+        self.assertEqual(n1, n2.prev)
+        self.assertIsNone(n2.next)
 
     def test_delete_sort(self):
         list = OrderedList(False)
