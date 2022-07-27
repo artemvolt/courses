44f37c43198f3d585b592d3ebd0ca71b3920db34
Artem Volt <avoltornistyy@beeline.ru>
2022-07-27 07:22:28 +0300

    Ordered list.

---
+++ courses/first/ordered_list/ordered_list.py
@@ -0,0 +1,147 @@
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.prev = None
+        self.next = None
+
+
+# noinspection PyComparisonWithNone,Pylint,PyPep8
+class OrderedList:
+    def __init__(self, asc):
+        self.head = None
+        self.tail = None
+        self.__ascending = asc
+
+    def compare(self, v1, v2):
+        if v1 == v2:
+            return 0
+
+        return 1 if v1 > v2 else -1
+        # -1 если v1 < v2
+        # 0 если v1 == v2
+        # +1 если v1 > v2
+
+    def add(self, value):
+        new_node = Node(value)
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+
+        if self.__ascending == (self.compare(new_node.value, self.head.value) < 0):
+            head = self.head
+            self.head = new_node
+            new_node.next = head
+            head.prev = new_node
+            return
+
+        if self.__ascending == (self.compare(new_node.value, self.tail.value) >= 0):
+            tail = self.tail
+            new_node.prev = tail
+            tail.next = new_node
+            self.tail = new_node
+            return
+
+        node = self.head
+        while next is not None:
+            if self.__ascending:
+                if self.compare(new_node.value, node.value) >= 0 and self.compare(new_node.value, node.next.value) <= 0:
+                    current = node
+                    current_next = node.next
+                    current.next = new_node
+                    new_node.prev = current
+                    new_node.next = current_next
+                    current_next.prev = new_node
+                    break
+
+            if not self.__ascending:
+                if self.compare(new_node.value, node.value) <= 0 and self.compare(new_node.value, node.next.value) >= 0:
+                    current = node
+                    current_next = node.next
+                    current.next = new_node
+                    new_node.prev = current
+                    new_node.next = current_next
+                    current_next.prev = new_node
+                    break
+
+            node = node.next
+        # автоматическая вставка value
+        # в нужную позицию
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            if self.__ascending is True and (self.compare(node.value, val) > 0):
+                return None
+            if self.__ascending is False and (self.compare(node.value, val) < 0):
+                return None
+            node = node.next
+        return None  # здесь будет ваш код
+
+    def delete(self, val):
+        node = self.head
+        if node is None:
+            return
+        if self.len() == 1:
+            if node.value == val:
+                self.clean(self.__ascending)
+            return
+
+        while node is not None:
+            prev = node.prev
+            next = node.next
+
+            if node.value == val:
+                # head
+                if prev is None:
+                    self.head = next
+                    next.prev = None
+                    node = node.next
+                    continue
+
+                # tail
+                if next is None:
+                    self.tail = prev
+                    prev.next = None
+                    node = node.next
+                    continue
+
+                prev.next = next
+                next.prev = prev
+
+            node = node.next
+        # здесь будет ваш код
+
+    def clean(self, asc):
+        self.head = None
+        self.tail = None
+        self.__ascending = asc
+        # здесь будет ваш код
+
+    def len(self):
+        node = self.head
+        count = 0
+        while node is not None:
+            count += 1
+            node = node.next
+        return count  # здесь будет ваш код
+
+    def get_all(self):
+        r = []
+        node = self.head
+        while node != None:
+            r.append(node)
+            node = node.next
+        return r
+
+
+# noinspection PyCompatibility
+class OrderedStringList(OrderedList):
+    def __init__(self, asc):
+        super(OrderedStringList, self).__init__(asc)
+
+    def compare(self, v1, v2):
+        # переопределённая версия для строк
+        return super().compare(v1.strip(), v2.strip())
---
+++ tests/courses/first/ordered_list/ordered_list_test.py
@@ -0,0 +1,239 @@
+import sys
+import unittest
+
+from courses.first.ordered_list.ordered_list import OrderedList, OrderedStringList
+
+sys.path.append('/var/www/courses')
+
+
+# noinspection DuplicatedCode
+class ordered_list_test(unittest.TestCase):
+    def test_compare(self):
+        list = OrderedList(True)
+        self.assertEqual(-1, list.compare(1, 2))
+        self.assertEqual(1, list.compare(2, 1))
+        self.assertEqual(0, list.compare(1, 1))
+
+    def test_add_asc(self):
+        list = OrderedList(True)
+        list.add(2)
+        self.assertEqual(2, list.head.value)
+        self.assertEqual(2, list.tail.value)
+        list.add(4)
+        self.assertEqual(2, list.head.value)
+        self.assertEqual(4, list.head.next.value)
+        self.assertEqual(4, list.tail.value)
+        self.assertEqual(2, list.tail.prev.value)
+        list.add(3)
+        # 2 - 3 - 4
+        n1 = list.head
+        n2 = n1.next
+        n3 = n2.next
+        self.assertEqual(2, n1.value)
+        self.assertEqual(3, n2.value)
+        self.assertEqual(4, n3.value)
+        self.assertEqual(n1, list.head)
+        self.assertEqual(2, n1.value)
+        self.assertEqual(n2, n1.next)
+        self.assertEqual(None, n1.prev)
+        self.assertEqual(n1, n2.prev)
+        self.assertEqual(n3, n2.next)
+        self.assertEqual(n2, n3.prev)
+        self.assertEqual(list.tail, n3)
+        # 2 - 3 - 4 - 5
+        list.add(5)
+        n1 = list.head
+        n2 = n1.next
+        n3 = n2.next
+        n4 = n3.next
+        self.assertEqual(2, n1.value)
+        self.assertEqual(3, n2.value)
+        self.assertEqual(4, n3.value)
+        self.assertEqual(5, n4.value)
+        self.assertEqual(list.head, n1)
+        self.assertEqual(None, n1.prev)
+        self.assertEqual(n2, n1.next)
+        self.assertEqual(n1, n2.prev)
+        self.assertEqual(n3, n2.next)
+        self.assertEqual(n2, n3.prev)
+        self.assertEqual(n4, n3.next)
+        self.assertEqual(n3, n4.prev)
+        self.assertEqual(None, n4.next)
+        self.assertEqual(list.tail, n4)
+
+    def test_add_desc(self):
+        list = OrderedList(False)
+        list.add(2)
+        self.assertEqual(2, list.head.value)
+        self.assertEqual(2, list.tail.value)
+        list.add(4)
+        n1 = list.head
+        n2 = n1.next
+        self.assertEqual(4, n1.value)
+        self.assertEqual(2, n2.value)
+        self.assertEqual(n2, n1.next)
+        self.assertEqual(n1, list.head)
+        self.assertEqual(n2, list.tail)
+
+        list.add(3)
+        # 4 - 3 - 2
+        n1 = list.head
+        n2 = n1.next
+        n3 = n2.next
+        self.assertEqual(4, n1.value)
+        self.assertEqual(3, n2.value)
+        self.assertEqual(2, n3.value)
+        self.assertEqual(n1, list.head)
+        self.assertEqual(n2, n1.next)
+        self.assertEqual(None, n1.prev)
+        self.assertEqual(n1, n2.prev)
+        self.assertEqual(n3, n2.next)
+        self.assertEqual(n2, n3.prev)
+        self.assertEqual(list.tail, n3)
+        # 5 - 4 - 3 - 2
+        list.add(5)
+        n1 = list.head
+        n2 = n1.next
+        n3 = n2.next
+        n4 = n3.next
+        self.assertEqual(5, n1.value)
+        self.assertEqual(4, n2.value)
+        self.assertEqual(3, n3.value)
+        self.assertEqual(2, n4.value)
+        self.assertEqual(list.head, n1)
+        self.assertEqual(None, n1.prev)
+        self.assertEqual(n2, n1.next)
+        self.assertEqual(n1, n2.prev)
+        self.assertEqual(n3, n2.next)
+        self.assertEqual(n2, n3.prev)
+        self.assertEqual(n4, n3.next)
+        self.assertEqual(n3, n4.prev)
+        self.assertEqual(None, n4.next)
+        self.assertEqual(list.tail, n4)
+
+    def test_strings(self):
+        list = OrderedStringList(True)
+        list.add('abc')
+        self.assertEqual('abc', list.head.value)
+        list.add('bca')
+        list.add('gfg')
+        n1 = list.head
+        n2 = list.head.next
+        n3 = n2.next
+        self.assertEqual('abc', n1.value)
+        self.assertEqual('bca', n2.value)
+        self.assertEqual('gfg', n3.value)
+        self.assertEqual(n1, list.head)
+        self.assertEqual(n2, list.head.next)
+        self.assertEqual(n1, n2.prev)
+        self.assertEqual(n3, n2.next)
+        self.assertEqual(n2, n3.prev)
+        self.assertEqual(n3, list.tail)
+
+    def test_strings_desc(self):
+        list = OrderedStringList(False)
+        list.add('abc')
+        self.assertEqual('abc', list.head.value)
+        list.add('bca')
+        list.add('gfg')
+        n1 = list.head
+        n2 = list.head.next
+        n3 = n2.next
+        self.assertEqual('gfg', n1.value)
+        self.assertEqual('bca', n2.value)
+        self.assertEqual('abc', n3.value)
+        self.assertEqual(n1, list.head)
+        self.assertEqual(n2, list.head.next)
+        self.assertEqual(n1, n2.prev)
+        self.assertEqual(n3, n2.next)
+        self.assertEqual(n2, n3.prev)
+        self.assertEqual(n3, list.tail)
+
+    def test_string_compare(self):
+        list = OrderedStringList(True)
+        self.assertEqual(-1, list.compare('abc', 'bca'))
+        self.assertEqual(0, list.compare('abc', 'abc'))
+        self.assertEqual(1, list.compare('bca', 'abc'))
+
+    def test_find(self):
+        list = OrderedList(True)
+        self.assertEqual(None, list.find(1))
+        list.add(1)
+        list.add(3)
+        list.add(4)
+        # 1 - 3 - 4
+        self.assertEqual(1, list.find(1).value)
+        self.assertEqual(3, list.find(3).value)
+        self.assertIsNone(list.find(2))
+
+        list = OrderedList(False)
+        self.assertEqual(None, list.find(1))
+        list.add(1)
+        list.add(3)
+        list.add(4)
+        # 4 - 3 - 1
+        self.assertEqual(1, list.find(1).value)
+        self.assertIsNone(list.find(2))
+        self.assertEqual(3, list.find(3).value)
+        self.assertEqual(4, list.find(4).value)
+        self.assertEqual(None, list.find(5))
+
+    def test_size(self):
+        list = OrderedList(True)
+        self.assertEqual(0, list.len())
+        list.add(1)
+        self.assertEqual(1, list.len())
+        list.add(2)
+        self.assertEqual(2, list.len())
+
+    def test_delete(self):
+        list = OrderedList(True)
+        list.add(1)
+        list.delete(1)
+        self.assertEqual(0, list.len())
+        self.assertIsNone(list.head)
+        self.assertIsNone(list.tail)
+        list.add(1)
+        list.add(2)
+        list.delete(1)
+        self.assertEqual(2, list.head.value)
+        self.assertEqual(2, list.tail.value)
+        list.add(1)
+        list.delete(2)
+        self.assertEqual(1, list.head.value)
+        self.assertEqual(1, list.tail.value)
+        list.add(2)
+        list.add(3)
+
+        # 1 - 3
+        list.delete(2)
+        n1 = list.head
+        n2 = n1.next
+        self.assertEqual(1, n1.value)
+        self.assertEqual(3, n2.value)
+
+    def test_delete_sort(self):
+        list = OrderedList(False)
+        list.add(1)
+        list.delete(1)
+        self.assertEqual(0, list.len())
+        self.assertIsNone(list.head)
+        self.assertIsNone(list.tail)
+        list.add(1)
+        list.add(2)
+        list.delete(1)
+        self.assertEqual(2, list.head.value)
+        self.assertEqual(2, list.tail.value)
+        list.add(1)
+        list.delete(2)
+        self.assertEqual(1, list.head.value)
+        self.assertEqual(1, list.tail.value)
+        list.add(2)
+        list.add(3)
+
+        # 3 - 1
+        list.delete(2)
+        n1 = list.head
+        n2 = n1.next
+        self.assertEqual(3, n1.value)
+        self.assertEqual(1, n2.value)
