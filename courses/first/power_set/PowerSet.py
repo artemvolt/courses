795d39a7aa422e404ad2a79c2124b2b917f4ffb1
Artem Volt <avoltornistyy@beeline.ru>
2022-08-02 23:40:01 +0300

    Power set.

---
+++ courses/first/power_set/PowerSet.py
@@ -0,0 +1,69 @@
+# наследуйте этот класс от HashTable
+# или расширьте его методами из HashTable
+class PowerSet:
+
+    def __init__(self):
+        # ваша реализация хранилища
+        self.values = []
+
+    def size(self):
+        return len(self.values)
+        # количество элементов в множестве
+
+    def put(self, value):
+        if not value in self.values:
+            self.values.append(value)
+
+    def get(self, value):
+        return value in self.values
+
+    def remove(self, value):
+        if self.get(value):
+            self.values.remove(value)
+            return True
+        return False
+
+    def intersection(self, set2):
+        # пересечение текущего множества и set2
+        set_new = PowerSet()
+        bigger = self if self.size() >= set2.size() else set2
+        smaller = set2 if self.size() >= set2.size() else self
+        for el in smaller.values:
+            if bigger.get(el):
+                set_new.put(el)
+
+        return set_new
+
+    def union(self, set2):
+        # объединение текущего множества и set2
+        set_new = PowerSet()
+        for el in self.values:
+            set_new.put(el)
+
+        for el2 in set2.values:
+            set_new.put(el2)
+
+        return set_new
+
+    def difference(self, set2):
+        set_new = PowerSet()
+        for el in self.values:
+            if set2.get(el) is False:
+                set_new.put(el)
+
+        for el2 in set2.values:
+            if self.get(el2) is False:
+                set_new.put(el2)
+        # разница текущего множества и set2
+        return set_new
+
+    def issubset(self, set2):
+        if self.size() >= set2.size():
+            for el in set2.values:
+                if not self.get(el):
+                    return False
+            return True
+        # возвращает True, если set2 есть
+        # подмножество текущего множества,
+        # иначе False
+        return False
---
+++ tests/courses/first/power_set/PowerSetTest.py
@@ -0,0 +1,174 @@
+import sys
+import unittest
+
+from courses.first.power_set.PowerSet import PowerSet
+
+sys.path.append('/var/www/courses')
+
+
+# noinspection DuplicatedCode
+class PowerSetTest(unittest.TestCase):
+    def test_size(self):
+        set = PowerSet()
+        self.assertEqual(0, set.size())
+        set.put(1)
+        self.assertEqual(1, set.size())
+        set.put(2)
+        self.assertEqual(2, set.size())
+        set.remove(2)
+        self.assertEqual(1, set.size())
+        set.remove(1)
+        self.assertEqual(0, set.size())
+
+    def test_put(self):
+        set = PowerSet()
+        set.put(1)
+        self.assertTrue(1 in set.values)
+        set.put(2)
+        self.assertTrue(2 in set.values)
+        set.put(2)
+        self.assertTrue(2 in set.values)
+        self.assertEqual(2, set.size())
+
+    def test_get(self):
+        set = PowerSet()
+        self.assertFalse(set.get(1))
+        set.put(1)
+        self.assertTrue(set.get(1))
+        set.put(2)
+        self.assertTrue(set.get(2))
+
+    def test_remove(self):
+        set = PowerSet()
+        self.assertFalse(set.remove(1))
+        set.put(1)
+        self.assertTrue(set.remove(1))
+        set.put(2)
+        self.assertTrue(set.remove(2))
+        self.assertFalse(set.remove(2))
+
+    def test_intersection_none(self):
+        set1 = PowerSet()
+        set1.put(1)
+        set1.put(2)
+        set1.put(3)
+        set2 = PowerSet()
+        set2.put(4)
+        set2.put(5)
+        set2.put(6)
+        set3 = set1.intersection(set2)
+        self.assertEqual(0, set3.size())
+
+    def test_intersection_one(self):
+        set1 = PowerSet()
+        set1.put(1)
+        set1.put(2)
+        set1.put(3)
+        set1.put(4)
+        set2 = PowerSet()
+        set2.put(4)
+        set2.put(5)
+        set2.put(6)
+        set3 = set1.intersection(set2)
+        self.assertEqual(1, set3.size())
+        self.assertTrue(set3.get(4))
+
+    def test_intersection_one_revert(self):
+        set1 = PowerSet()
+        set1.put(1)
+        set1.put(2)
+        set1.put(3)
+        set2 = PowerSet()
+        set2.put(3)
+        set2.put(4)
+        set2.put(5)
+        set2.put(6)
+        set3 = set1.intersection(set2)
+        self.assertEqual(1, set3.size())
+        self.assertTrue(set3.get(3))
+
+    def test_intersection_union(self):
+        set1 = PowerSet()
+        set1.put(1)
+        set1.put(2)
+        set1.put(3)
+        set2 = PowerSet()
+        set2.put(3)
+        set2.put(4)
+        set2.put(5)
+        set2.put(6)
+        set3 = set1.union(set2)
+        self.assertEqual(6, set3.size())
+        self.assertTrue(set3.get(1))
+        self.assertTrue(set3.get(2))
+        self.assertTrue(set3.get(3))
+        self.assertTrue(set3.get(4))
+        self.assertTrue(set3.get(5))
+        self.assertTrue(set3.get(6))
+
+    def test_intersection_difference(self):
+        set1 = PowerSet()
+        set1.put(1)
+        set1.put(2)
+        set1.put(3)
+        set2 = PowerSet()
+        set2.put(3)
+        set2.put(4)
+        set2.put(5)
+        set2.put(6)
+        set3 = set1.difference(set2)
+        self.assertEqual(5, set3.size())
+
+    def test_intersection_difference_same(self):
+        set1 = PowerSet()
+        set1.put(1)
+        set1.put(2)
+        set1.put(3)
+        set2 = PowerSet()
+        set2.put(1)
+        set2.put(2)
+        set2.put(3)
+        set3 = set1.difference(set2)
+        self.assertEqual(0, set3.size())
+
+    def test_intersection_difference_same_empty(self):
+        set1 = PowerSet()
+        set2 = PowerSet()
+        set3 = set1.difference(set2)
+        self.assertEqual(0, set3.size())
+
+    def test_intersection_issubset_same(self):
+        set1 = PowerSet()
+        set1.put(1)
+        set1.put(2)
+        set1.put(3)
+        set2 = PowerSet()
+        set2.put(1)
+        set2.put(2)
+        set2.put(3)
+        self.assertTrue(set1.issubset(set2))
+
+    def test_intersection_issubset_bigger_with_revert(self):
+        set1 = PowerSet()
+        set1.put(1)
+        set1.put(2)
+        set1.put(3)
+        set1.put(4)
+        set2 = PowerSet()
+        set2.put(1)
+        set2.put(2)
+        set2.put(3)
+        self.assertTrue(set1.issubset(set2))
+        self.assertFalse(set2.issubset(set1))
+
+    def test_intersection_issubset_bigger(self):
+        set1 = PowerSet()
+        set1.put(1)
+        set1.put(2)
+        set1.put(3)
+        set2 = PowerSet()
+        set2.put(1)
+        set2.put(2)
+        set2.put(3)
+        set2.put(4)
+        self.assertTrue(set2.issubset(set1))
