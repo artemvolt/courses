8a892a9e27c4d817b25ae65a8631ae4b74b69d6f
Artem Volt <avoltornistyy@beeline.ru>
2022-10-30 11:52:14 +0300

    Change code of functions.

--- courses/third_recursion/nine_balance_parentheses.py
+++ courses/third_recursion/nine_balance_parentheses.py
@@ -6,24 +6,18 @@ def balance_parentheses(count: int) -> str:
     if count < 1:
         raise ArithmeticError("Count must be positive")
 
-    return add_balance(count, '()')
+    return add_balance(count, '')
 
 
 def add_balance(count: int, _input: str) -> str:
-    if len(_input) / 2 == count:
+    length = len(_input)
+    if length / 2 == count:
         return _input
 
-    to_list = list(_input)
-    random_position = random.choice(get_indexes(to_list))
-    to_list.insert(random_position, ')')
-    to_list.insert(random_position, '(')
+    max_index = length - 1 if length > 1 else 0
+    random_index = random.randint(0, max_index)
 
-    return add_balance(count, ''.join(map(lambda x: x, to_list)))
+    before = _input[:random_index]
+    after = _input[random_index:]
 
-
-def get_indexes(_input: list) -> list:
-    """Получить индексы массива"""
-    result = []
-    for index, value in enumerate(_input):
-        result.append(index)
-    return result
+    return add_balance(count, before + '()' + after)
