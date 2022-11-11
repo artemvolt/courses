80ce782ce02400f35a20ceedf1f0aa9732434428
Artem Volt <avoltornistyy@beeline.ru>
2022-11-11 12:19:25 +0300

    Binary search three.

---
+++ courses/first_algor_part_2/binary_search_trees/BinarySearchTree.py
@@ -0,0 +1,112 @@
+class BSTNode:
+
+    def __init__(self, key, val, parent):
+        self.NodeKey = key  # ключ узла
+        self.NodeValue = val  # значение в узле
+        self.Parent = parent  # родитель или None для корня
+        self.LeftChild = None  # левый потомок
+        self.RightChild = None  # правый потомок
+
+
+class BSTFind:  # промежуточный результат поиска
+
+    def __init__(self):
+        self.Node = None  # None если
+        # в дереве вообще нету узлов
+
+        self.NodeHasKey = False  # True если узел найден
+        self.ToLeft = False  # True, если родительскому узлу надо
+        # добавить новый узел левым потомком
+
+
+class BST:
+
+    def __init__(self, node: BSTNode = None):
+        self.Root = node  # корень дерева, или None
+        self.count = 0
+
+    def FindNodeByKeyRecursive(self, key, node: BSTNode) -> BSTFind:
+        if key < node.NodeKey and node.LeftChild is not None:
+            return self.FindNodeByKeyRecursive(key, node.LeftChild)
+
+        if key > node.NodeKey and node.RightChild is not None:
+            return self.FindNodeByKeyRecursive(key, node.RightChild)
+
+        result = BSTFind()
+        result.Node = node
+        if key == node.NodeKey:
+            result.NodeHasKey = True
+
+        if key < node.NodeKey:
+            result.ToLeft = True
+
+        return result
+
+    def FindNodeByKey(self, key):
+        # ищем в дереве узел и сопутствующую информацию по ключу
+        if self.Root is None:
+            return BSTFind()
+        return self.FindNodeByKeyRecursive(key, self.Root)
+
+    def AddKeyValue(self, key, val) -> bool:
+        # добавляем ключ-значение в дерево
+        found = self.FindNodeByKey(key)
+        if found.NodeHasKey:
+            return False
+
+        node = found.Node
+        new = BSTNode(key, val, node)
+        if node is None:
+            self.Root = new
+        elif found.ToLeft is True:
+            node.LeftChild = new
+        else:
+            node.RightChild = new
+
+        return True  # если ключ уже есть
+
+    def FinMinMax(self, FromNode: BSTNode, FindMax: bool) -> BSTNode:
+        # ищем максимальный/минимальный ключ в поддереве
+        # возвращается объект типа BSTNode
+        right = FromNode.RightChild
+        left = FromNode.LeftChild
+        node = right if FindMax is True else left
+        if node is None:
+            return FromNode
+        return self.FinMinMax(node, FindMax)
+
+    def DeleteNodeByKey(self, key):
+        # удаляем узел по ключу
+        found = self.FindNodeByKey(key)
+        if found.NodeHasKey:
+            node = found.Node
+            parent = node.Parent
+            new_node = self.FinMinMax(node, True)
+            if node is new_node:
+                if node is parent.LeftChild:
+                    parent.LeftChild = None
+                if node is parent.RightChild:
+                    parent.RightChild = None
+                return True
+
+            if node is parent.LeftChild:
+                parent.LeftChild = new_node
+                return True
+            if node is parent.RightChild:
+                parent.RightChild = new_node
+                return True
+        return False  # если узел не найден
+
+    def Count(self) -> int:
+        self.count = 0
+        self.Count_Recursive(self.Root)
+        return self.count
+
+    def Count_Recursive(self, node):
+        if node is None:
+            return self.count
+        self.count += 1
+        if node.LeftChild is not None:
+            self.Count_Recursive(node.LeftChild)
+        if node.RightChild is not None:
+            self.Count_Recursive(node.RightChild)
---
+++ tests/courses/first_algor_part_2/binary_search_trees/BinarySearchTreeTest.py
@@ -0,0 +1,189 @@
+import unittest
+import sys
+
+from courses.first_algor_part_2.binary_search_trees.BinarySearchTree import BST, BSTFind, BSTNode
+
+sys.path.append('/var/www/courses')
+
+
+class BinarySearchTreeTest(unittest.TestCase):
+
+    def create_tree(self):
+        first = BSTNode(8, 8, None)
+        first.LeftChild = fourth = BSTNode(4, 4, first)
+        first.RightChild = twelve = BSTNode(12, 12, first)
+
+        fourth.LeftChild = two = BSTNode(2, 2, fourth)
+        fourth.RightChild = six = BSTNode(6, 6, fourth)
+
+        twelve.LeftChild = ten = BSTNode(10, 10, twelve)
+        twelve.RightChild = fourtheen = BSTNode(14, 14, twelve)
+
+        tree = BST(first)
+        return [tree, first, fourth, twelve, two, six, ten, fourtheen]
+
+    def test_find(self):
+        tree = BST(None)
+        result = tree.FindNodeByKey(0)
+        self.assertTrue(result.Node is None)
+
+        # existing key
+        testKey = 0
+        node = BSTNode(testKey, testKey, None)
+        tree = BST(node)
+        result = tree.FindNodeByKey(testKey)
+        self.assertTrue(result.Node is node)
+        self.assertTrue(result.NodeHasKey is True)
+
+        # search existing in right
+        [tree, first, fourth, twelve, two, six, ten, fourtheen] = self.create_tree()
+        result = tree.FindNodeByKey(14)
+        self.assertTrue(result.Node is fourtheen)
+        self.assertTrue(result.NodeHasKey is True)
+
+        # search exsiting in left
+        result = tree.FindNodeByKey(2)
+        self.assertTrue(result.Node is two)
+        self.assertTrue(result.NodeHasKey is True)
+
+        # search none exsiting in left
+        result = tree.FindNodeByKey(1)
+        self.assertEqual(2, result.Node.NodeKey)
+        self.assertTrue(result.NodeHasKey is False)
+        self.assertTrue(result.ToLeft is True)
+
+        # search none exsiting in right
+        result = tree.FindNodeByKey(13)
+        self.assertEqual(14, result.Node.NodeKey)
+        self.assertTrue(result.NodeHasKey is False)
+        self.assertTrue(result.ToLeft is True)
+
+        # search none exsiting in right
+        result = tree.FindNodeByKey(15)
+        self.assertEqual(14, result.Node.NodeKey)
+        self.assertTrue(result.NodeHasKey is False)
+        self.assertTrue(result.ToLeft is False)
+
+    def test_add(self):
+        tree = BST()
+        self.assertFalse(tree.FindNodeByKey(8).NodeHasKey)
+        self.assertTrue(
+            tree.AddKeyValue(8, 8)
+        )
+
+        root = tree.FindNodeByKey(8).Node
+        self.assertTrue(tree.FindNodeByKey(8).NodeHasKey)
+        self.assertEqual(root, tree.Root)
+        self.assertEqual(8, tree.Root.NodeKey)
+
+        self.assertFalse(tree.FindNodeByKey(12).NodeHasKey)
+        self.assertTrue(
+            tree.AddKeyValue(12, 12)
+        )
+        twelveFound = tree.FindNodeByKey(12)
+        twelve = twelveFound.Node
+        self.assertTrue(twelveFound.NodeHasKey)
+        self.assertEqual(twelve, root.RightChild)
+        self.assertIsNone(root.LeftChild)
+        self.assertEqual(12, twelve.NodeValue)
+
+        self.assertFalse(tree.FindNodeByKey(4).NodeHasKey)
+        self.assertTrue(
+            tree.AddKeyValue(4, 4)
+        )
+        foundFour = tree.FindNodeByKey(4)
+        four = foundFour.Node
+        self.assertTrue(tree.FindNodeByKey(4).NodeHasKey)
+        self.assertEqual(four, root.LeftChild)
+        self.assertEqual(twelve, root.RightChild)
+
+        self.assertFalse(
+            tree.AddKeyValue(4, 4)
+        )
+        self.assertFalse(
+            tree.AddKeyValue(12, 12)
+        )
+        self.assertEqual(tree.Root, root)
+        self.assertEqual(twelve, root.RightChild)
+        self.assertEqual(four, root.LeftChild)
+
+        self.assertFalse(tree.FindNodeByKey(2).NodeHasKey)
+        self.assertTrue(
+            tree.AddKeyValue(2, 2)
+        )
+        foundTwo = tree.FindNodeByKey(2)
+        two = foundTwo.Node
+        self.assertTrue(foundTwo.NodeHasKey)
+        self.assertEqual(two, four.LeftChild)
+        self.assertFalse(
+            tree.AddKeyValue(2, 2)
+        )
+
+        self.assertFalse(tree.FindNodeByKey(6).NodeHasKey)
+        self.assertTrue(
+            tree.AddKeyValue(6, 6)
+        )
+        foundSix = tree.FindNodeByKey(6)
+        six = foundSix.Node
+        self.assertTrue(foundSix.NodeHasKey)
+        self.assertEqual(six, four.RightChild)
+        self.assertFalse(
+            tree.AddKeyValue(6, 6)
+        )
+
+        self.assertFalse(tree.FindNodeByKey(10).NodeHasKey)
+        self.assertTrue(
+            tree.AddKeyValue(10, 10)
+        )
+        foundTen = tree.FindNodeByKey(10)
+        ten = foundTen.Node
+        self.assertTrue(foundTen.NodeHasKey)
+        self.assertEqual(ten, twelve.LeftChild)
+
+        self.assertFalse(tree.FindNodeByKey(9).NodeHasKey)
+        self.assertTrue(
+            tree.AddKeyValue(9, 9)
+        )
+        foundNine = tree.FindNodeByKey(9)
+        nine = foundNine.Node
+        self.assertTrue(foundNine.NodeHasKey)
+        self.assertEqual(nine, ten.LeftChild)
+
+    def test_find_max(self):
+        [tree, first, fourth, twelve, two, six, ten, fourtheen] = self.create_tree()
+        self.assertEqual(fourtheen, tree.FinMinMax(tree.Root, True))
+        self.assertEqual(two, tree.FinMinMax(tree.Root, False))
+
+        self.assertEqual(fourtheen, tree.FinMinMax(twelve, True))
+        self.assertEqual(ten, tree.FinMinMax(twelve, False))
+
+        self.assertEqual(six, tree.FinMinMax(fourth, True))
+        self.assertEqual(two, tree.FinMinMax(fourth, False))
+
+        self.assertEqual(ten, tree.FinMinMax(ten, True))
+        self.assertEqual(ten, tree.FinMinMax(ten, False))
+
+    def test_count(self):
+        tree = BST()
+        self.assertEqual(0, tree.Count())
+        [tree, first, fourth, twelve, two, six, ten, fourtheen] = self.create_tree()
+        self.assertEqual(7, tree.Count())
+
+    def test_delete(self):
+        [tree, first, fourth, twelve, two, six, ten, fourtheen] = self.create_tree()
+        self.assertTrue(tree.FindNodeByKey(10).NodeHasKey)
+        tree.DeleteNodeByKey(10)
+        self.assertFalse(tree.FindNodeByKey(10).NodeHasKey)
+        self.assertEqual(first, tree.Root)
+        self.assertEqual(fourth, first.LeftChild)
+        self.assertEqual(twelve, first.RightChild)
+        self.assertEqual(None, twelve.LeftChild)
+        self.assertEqual(fourtheen, twelve.RightChild)
+
+        self.assertTrue(tree.FindNodeByKey(4).NodeHasKey)
+        tree.DeleteNodeByKey(4)
+        self.assertFalse(tree.FindNodeByKey(4).NodeHasKey)
+        self.assertEqual(first, tree.Root)
+        self.assertEqual(six, first.LeftChild)
+        self.assertEqual(twelve, first.RightChild)
+
